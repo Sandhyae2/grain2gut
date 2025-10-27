afebed59ad2396f99531701aca63b06ea3f9915b
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-10-27 14:13:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.markdown("""
 <style>
 .stApp {
-    background-image: url('https://www.tugraz.at/fileadmin/_processed_/d/b/csm_Banner_Weizen-by-silentforce-AdobeStock_55e08033b4.jpg);  
+    background-image: url('https://www.tugraz.at/fileadmin/_processed_/d/b/csm_Banner_Weizen-by-silentforce-AdobeStock_55e08033b4.jpg');  
     background-size: cover;
     background-attachment: fixed;
 }
