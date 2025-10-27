e85b951f6d4b3f39c83cd803e8745f612cfec398
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-10-27 14:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,29 @@ st.markdown("""
     background-size: cover;
     background-attachment: fixed;
 }
+.block-container {
+    padding-top: 2rem;
+    padding-left: 6rem;
+    padding-right: 6rem;
+}
+h2, h1 {
+    text-align: center !important;
+    color: #2c3e50;
+}
+.stColumns {
+    gap: 40px !important;
+}
+.stButton>button {
+    background-color:#FEF7A2;
+    color:#2c3e50;
+    font-size:20px;
+    border-radius:10px;
+    padding:10px 20px;
+    border:none;
+    transition: background-color 0.3s ease;
+}
+.stButton>button:hover {
+    background-color:#DFFBB9;
+}
 </style>
 """, unsafe_allow_html=True)
