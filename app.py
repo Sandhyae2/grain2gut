2043d8038a56f5c955a2572fd9b83023d9cf73ea
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-10-27 14:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 import streamlit as st
+st.markdown("""
+<style>
+.stApp {
+    background-image: url('https://img.freepik.com/premium-vector/paddy-rice-field-background_267448-280.jpg');  
+    background-size: cover;
+    background-attachment: fixed;
+}
+</style>
+""", unsafe_allow_html=True)
