e348566381d65f23bb2a1226607f639e61b3f61e
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-10-27 14:10:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.markdown("""
 <style>
 .stApp {
-    background-image: url('https://img.freepik.com/premium-vector/paddy-rice-field-background_267448-280.jpg');  
+    background-image: url('https://media.istockphoto.com/id/119165809/photo/close-up-of-ripe-wheat-ears.jpg?s=612x612&w=0&k=20&c=HOUVeZyqJz_OOa_DNU6cUjaus_puQ9yS19SK952oIt0=');  
     background-size: cover;
     background-attachment: fixed;
 }
