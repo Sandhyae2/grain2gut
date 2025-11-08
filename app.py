8382479fb9b7ead285c93ab5cd9b0c16c1b104c7
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-11-08 21:30:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide",page_icon="🌾")
 st.markdown("""
 <style>
 .stApp {
-    background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK32OWFSdrVavAHDsiIeFidPdCa49dIZwYPQ&s');  
+    background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXPTzkCwuIrSVOWxjfUZfvrkPudpsQlADjbQ&s');  
     background-size: cover;
     background-attachment: fixed;
 }
