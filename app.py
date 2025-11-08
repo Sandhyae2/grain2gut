44b6e3ceceacdfc1b5df4c09fc76bc48442d56d8
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-11-08 19:58:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide",page_icon="🌾")
 st.markdown("""
 <style>
 .stApp {
-    background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dbarley%2Bgrain%2Band%2Bplant&psig=AOvVaw1-U_g0cUmpykh2EKsJUrW4&ust=1762697318007000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCNiRmtfe4pADFQAAAAAdAAAAABAE');  
+    background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Flush-green-wheat&psig=AOvVaw2VheyOoCYGNVIUsepZLMlU&ust=1762698317735000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCMDdt6Hh4pADFQAAAAAdAAAAABAE');  
     background-size: cover;
     background-attachment: fixed;
 }
