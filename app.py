f23c64e1ea71f47cfbc8d038221b50f992951d3d
Sandhya Elumalai <sandhyaelumalai76@gmail.com>
2025-11-10 15:56:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -193,20 +193,24 @@ def home():
            
 
 # ------------------------------------------------Millet-wise Analysis---------------------------------------------------------------
-    with middle_col:
-        if st.button("Summarized Analysis"):
-            go_to("summarized_analysis")
+   with middle_col:
+        with st.container(border=True):
+            if st.button("Summarized Analysis", use_container_width=True):
+                go_to("summarized_analysis")
+            st.write("Overall summary of the results.")
    
  # -------------------------------------------------- Meta Data ---------------------------------------------------------------------------
     with right_col:
         # Detailed Analysis heading
         st.markdown("<h4 style='text-align:center; margin-bottom:20px;'>Meta Data</h4>", unsafe_allow_html=True)
-        # Center buttons below the heading
-        for label, page_key in [("EC Analysis", "ec_analysis"), ("KO Analysis", "ko_analysis"), ("Pathway Analysis", "pwy_analysis")]:
-            col1, col2, col3 = st.columns([1, 2, 1])  # middle column holds the button
-            with col2:
-                if st.button(label, key=label):
-                    go_to(page_key)
+        with st.container(border=True):
+            st.write("Processed dataframes created from raw files for further analysis.")
+            # Center buttons below the heading
+            for label, page_key in [("EC Analysis", "ec_analysis"), ("KO Analysis", "ko_analysis"), ("Pathway Analysis", "pwy_analysis")]:
+                col1, col2, col3 = st.columns([1, 2, 1])  # middle column holds the button
+                with col2:
+                    if st.button(label, key=label):
+                        go_to(page_key)
     footer()
  # -------------------------------------------------- Millet Data Mapping -------------------------------------------------------------------
 millet_map = {
